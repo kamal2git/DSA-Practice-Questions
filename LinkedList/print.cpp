77af4aed98fe39738154b9bad7fe407fe296c47e
Kamal <kamalproacc@gmail.com>
2021-10-02 22:26:09 +0530

    print function added

---
+++ LinkedList/print.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include "node.cpp"
+void print(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data;
+        temp=temp->next;
+    }
+}
