5364b647eeabcbcf07a387b783e87ad3fc412bff
Kamal <kamalproacc@gmail.com>
2021-10-02 23:59:19 +0530

    added code for i=0 and i>n

--- LinkedList/addNodeithpos.cpp
+++ LinkedList/addNodeithpos.cpp
@@ -1,15 +1,23 @@
 #include<iostream>
 #include "node.cpp"
 using namespace std;
-void addNodeithpos(node* head, int data, int i){
+node* addNodeithpos(node* head, int data, int i){
     node* newNode=new node(data);
     node* temp=head;
+    if(i==0){
+        newNode->next=head;
+        head=newNode;
+        return head;
+    }
     int count=0;
-    while(count<i-1){
+    while(temp!=NULL && count<i-1){
         temp=temp->next;
         count++;
     }
-    node *a=temp->next;
+    if(temp!=NULL){
+        node *a=temp->next;
     temp->next=newNode;
     newNode->next=a;
+    }
+    return head;
 }
