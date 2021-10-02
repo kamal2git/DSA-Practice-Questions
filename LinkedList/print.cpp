50958f57a71c2c84f09f67ba25444339e054033e
Kamal <kamalproacc@gmail.com>
2021-10-02 22:28:13 +0530

    added print fn

--- LinkedList/print.cpp
+++ LinkedList/print.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print(node* head){
     node* temp=head;
     while(temp!=NULL){
-        cout<<temp->data;
+        cout<<temp->data<<" ";
         temp=temp->next;
     }
 }
