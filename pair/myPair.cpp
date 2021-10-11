8d84a8cf675a117d57c3994b1d1ec1f8959ee2f1
Kamal <kamalproacc@gmail.com>
2021-10-12 00:33:22 +0530

    trien overloading print fn

--- pair/myPair.cpp
+++ pair/myPair.cpp
@@ -17,22 +17,29 @@ class myPair{
     V getY(){
         return y;
     }
-    void print(){
-        cout<<"x: "<<x<<" and y: "<<y<<endl;
+    void print(myPair<int,int> p){
+        //cout<<"x: "<<x<<" and y: "<<y<<endl;
     }
 };
+void myPair::print(myPair<int,int> p){
+        cout<<"x: "<<p.getX()<<" and y: "<<p.getY()<<endl;
+    }
+}
+void myPair::print(myPair<myPair<int,int>,int> p){
+    cout<<p.getX().getX()<<" "<<p.getX().getY()<<" "<<p.getY<<endl;
+}
 int main(){
     myPair<int,char> p1;
     p1.setX(10);
     p1.setY('a');
     p1.print();
-    myPair<double,int> p2;
-    p2.setX(10.2);
-    p2.setY(10);
-    p2.print();
-    myPair<char,int> p3;
-    p3.setX('a');
-    p3.setY(2);
-    p3.print();
+    
+    myPair<myPair<int,int>,int> p4; //pair of pair
+    p4.setY(100);
+    myPair<int,int> p5;
+    p5.setX(200);
+    p5.setY(300);
+    p4.setX(p5);
+    //p4.print();          overload the print fn bro!
     return 0;
 }
