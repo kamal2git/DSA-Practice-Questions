db0ba47f44694fb5ea80afd29895bb3251150557
Kamal <kamalproacc@gmail.com>
2021-12-21 12:20:52 +0530

    fixed patches

--- stack/nearestGreaterToRight.cpp
+++ stack/nearestGreaterToRight.cpp
@@ -4,10 +4,13 @@ int main(){
     int size;
     cin>>size;
     int arr[size];
+    for(int k=0;k<size;k++){
+        cin>>arr[k];
+    }
     vector <int> v;
     stack <int> s;
     
-    for(int i=size;i>0;i++){
+    for(int i=size-1;i>=0;--i){
         if(s.size()==0){
             v.push_back(-1);
         }
@@ -15,21 +18,23 @@ int main(){
         {
             v.push_back(s.top());
         }
-        else if(s.size>0 && s.top() <= arr[i];)
+        else if(s.size()>0 && s.top() <= arr[i])
         {
             while(s.size()>0 && s.top()<=arr[i]){
                 s.pop();
-                if(s.size()==0){
+            }
+            if(s.size()==0){
                     v.push_back(-1);
                 }
-                else {
+                else if(s.size()>0 && s.top()>arr[i]) {
                     v.push_back(s.top());
                 }
-            }
         }
         s.push(arr[i]);
     }
+    reverse(v.begin(),v.end());
+    for(int j=0;j<v.size();j++){
+        cout<<v.at(j)<<" ";
+    }
+    return 0;
 }
-
-
-
